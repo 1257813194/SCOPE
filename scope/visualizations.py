829c605d97a4ef01875f41b947cef7e5d8f5f27c
Jiamin Zou <jmzou@umich.edu>
2026-08-13 18:18:14 -0400

    update code

--- scope/visualizations.py
+++ scope/visualizations.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import torch
+import seaborn as sns
 from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
@@ -15,13 +16,13 @@ def visualize_full_evolution(
     edges: List[Tuple[Any, Any, float, str]],
     time_labels: Optional[List[str]] = None,
     umap_model: Optional[Any] = None,
-    legend_fontsize: int = 10,
+    legend_fontsize: int = 14,
     save_path: Optional[str] = None,
     adjust_upside_down: Optional[bool] = False
 ):
     
     num_timesteps = len(centers)
-    fig, ax = plt.subplots(figsize=(12, 12))
+    fig, ax = plt.subplots(figsize=(10, 10))
     colors = cm.viridis(np.linspace(0, 1, num_timesteps))
 
     if time_labels is None:
@@ -65,28 +66,29 @@ def visualize_full_evolution(
 
     for t in range(num_timesteps):
         if t < len(raw_data_2d) and raw_data_2d[t].size > 0:
-            ax.scatter(raw_data_2d[t][:, 0], raw_data_2d[t][:, 1], color=colors[t], s=10, alpha=0.2, zorder=1)
+            ax.scatter(raw_data_2d[t][:, 0], raw_data_2d[t][:, 1], color=colors[t], s=18, alpha=0.35, zorder=1)
     edge_styles = {'backbone': {'color': 'black', 'linestyle': '-'},'rescue_child': {'color': '#0077b6', 'linestyle': '--'},'rescue_parent': {'color': '#d00000', 'linestyle': ':'}}
     for u, v, weight, edge_type in edges:
         pos_u, pos_v = all_points_map_2d.get(u), all_points_map_2d.get(v)
         if pos_u is not None and pos_v is not None:
             style = edge_styles.get(edge_type, {'color': 'gray', 'linestyle': ':'})
-            ax.plot([pos_u[0], pos_v[0]], [pos_u[1], pos_v[1]], **style, linewidth=1.5, alpha=0.8, zorder=2)
+            ax.plot([pos_u[0], pos_v[0]], [pos_u[1], pos_v[1]], **style, linewidth=2.5, alpha=0.9, zorder=2)
     for t in range(num_timesteps):
         if t < len(centers_2d) and centers_2d[t].size > 0:
             ax.scatter(centers_2d[t][:, 0], centers_2d[t][:, 1], color=colors[t], s=100, ec='white', linewidth=1.5, zorder=3, label=f'{time_labels[t]}')
 
     ax.set_title('Evolutionary Graph of Cell States', fontsize=16)
-    ax.set_xlabel('Dimension 1 (UMAP or Original)', fontsize=12)
-    ax.set_ylabel('Dimension 2 (UMAP or Original)', fontsize=12)
+    ax.set_xlabel('Dimension 1 (UMAP or Original)', fontsize=14)
+    ax.set_ylabel('Dimension 2 (UMAP or Original)', fontsize=14)
 
 
     legend_main = ax.legend(
-        title='Cluster Centers', 
         bbox_to_anchor=(1.02, 1), 
         loc='upper left',
         fontsize=legend_fontsize  
     )
+    legend_main.set_title("Cluster Centers", prop={"size": legend_fontsize})
+    
     ax.add_artist(legend_main)
     
     legend_elements = [
@@ -94,14 +96,17 @@ def visualize_full_evolution(
         Line2D([0], [0], color='#0077b6', linestyle='--', lw=2, label='Child Rescue'),
         Line2D([0], [0], color='#d00000', linestyle=':', lw=2, label='Parent Rescue')
     ]
-    ax.legend(
+
+    legend_edge = ax.legend(
         handles=legend_elements, 
-        title='Edge Types', 
         bbox_to_anchor=(1.02, 0), 
         loc='lower left',
         fontsize=legend_fontsize 
     )
+    
+    legend_edge.set_title("Edge Types",prop={"size": legend_fontsize})
 
+    
     ax.grid(True, linestyle='--', alpha=0.2)
     if save_path is not None:
         plt.savefig(
@@ -163,35 +168,37 @@ def visualize_generated_populations(
     umap_model: Optional[Any] = None,
     adjust_equal:Optional[bool] = False,
     save_path: Optional[str] = None,
-    adjust_upside_down: Optional[bool] = False
+    adjust_upside_down: Optional[bool] = False,
+    adjust_left_right: Optional[bool] = False,
     ):
     x_seq=torch.cat(raw_data).cpu().numpy()
     color_lst = ['#377EB8','#D35400','#8E44AD']
-    label_lst= ['WM','Layer 6','Layer 5','Layer 4','Layer 3','Layer 2','Layer 1']
     step_each = (len(generated_data)-1)//(observed_time_points[-1] - observed_time_points[0])
     for t in range(1,len(raw_data)):
-        fig, ax = plt.subplots(figsize=(6, 6))
+        fig, ax = plt.subplots(figsize=(8, 8))
         cmap = plt.cm.tab20
         xu = umap_model.transform(x_seq) if umap_model is not None else x_seq
         xu_generated = umap_model.transform(generated_data[int(step_each*(observed_time_points[t] - observed_time_points[0]))].cpu().numpy()) if umap_model is not None else generated_data[int(step_each*(observed_time_points[t] - observed_time_points[0]))].cpu().numpy()
-        ax.scatter(xu[:,0], xu[:,1] if not adjust_upside_down else -xu[:,1] + 9, s=1, color = 'gray')
-        if len(raw_data) <=3:
-            for t_ in range(t+1):    
-                xu_ = umap_model.transform(raw_data[t_].cpu().numpy()) if umap_model is not None else raw_data[t_].cpu().numpy()
-                ax.scatter(x=xu_[:, 0], y=xu_[:, 1] if not adjust_upside_down else -xu_[:,1] + 9, s=1, label='real ' + str(observed_time_points[t_]), color=color_lst[t_], alpha=0.5)
-        else:
-            xu_ = umap_model.transform(raw_data[t].cpu().numpy()) if umap_model is not None else raw_data[t].cpu().numpy()
-            #ax.scatter(x=xu_[:, 0], y=xu_[:, 1] if not adjust_upside_down else -xu[:,1] + 9, s=1, label='real ' + str(observed_time_points[t]), color='#D35400', alpha=0.5)
-            ax.scatter(x=xu_[:, 0], y=xu_[:, 1] if not adjust_upside_down else -xu[:,1] + 9, s=1, label='real ' + label_lst[t], color='#D35400', alpha=0.5)
-        #ax.scatter(xu_generated[:,0], xu_generated[:,1] if not adjust_upside_down else -xu_generated[:,1] + 9, s = 1, color = '#FFBF00',label='fake ' + str(observed_time_points[t]))
-        ax.scatter(xu_generated[:,0], xu_generated[:,1] if not adjust_upside_down else -xu_generated[:,1] + 9, s = 1, color = '#FFBF00',label='fake ' + label_lst[t])
-        ax.legend(markerscale=3,loc="lower left")
+        ax.scatter(xu[:,0] if not adjust_left_right else -xu[:,0], xu[:,1] if not adjust_upside_down else -xu[:,1] + 9, s=1, color = 'gray')
+        # if len(raw_data) <=3:
+        #     for t_ in range(t+1):    
+        #         xu_ = umap_model.transform(raw_data[t_].cpu().numpy()) if umap_model is not None else raw_data[t_].cpu().numpy()
+        #         ax.scatter(x=xu_[:, 0], y=xu_[:, 1] if not adjust_upside_down else -xu_[:,1] + 9, s=1, label='real ' + str(observed_time_points[t_]), color=color_lst[t_], alpha=0.5)
+        # else:
+        #     xu_ = umap_model.transform(raw_data[t].cpu().numpy()) if umap_model is not None else raw_data[t].cpu().numpy()
+        #     ax.scatter(x=xu_[:, 0], y=xu_[:, 1] if not adjust_upside_down else -xu[:,1] + 9, s=1, label='real ' + str(observed_time_points[t]), color='#D35400', alpha=0.5)
+        # ax.scatter(xu_generated[:,0], xu_generated[:,1] if not adjust_upside_down else -xu_generated[:,1] + 9, s = 1, color = '#FFBF00',label='fake ' + str(observed_time_points[t]))
+        # ax.legend(markerscale=3,loc="lower left")
+        xu_ = umap_model.transform(raw_data[t].cpu().numpy()) if umap_model is not None else raw_data[t].cpu().numpy()
+        ax.scatter(x=xu_[:, 0] if not adjust_left_right else -xu_[:,0], y=xu_[:, 1] if not adjust_upside_down else -xu[:,1] + 9, s=1, label='real ' + str(observed_time_points[t]), color='gray')
+        ax.scatter(xu_generated[:,0] if not adjust_left_right else -xu_generated[:,0], xu_generated[:,1] if not adjust_upside_down else -xu_generated[:,1] + 9, s = 1, color = 'yellow',label='fake ' + str(observed_time_points[t]))
+        sns.kdeplot(x=xu_generated[:,0] if not adjust_left_right else -xu_generated[:,0], y=xu_generated[:,1] if not adjust_upside_down else -xu_generated[:,1] + 9, color = 'k')
         if adjust_equal:
             ax.set_aspect('equal', adjustable='box')
         if save_path is not None:
             plt.savefig(
-                save_path+str(observed_time_points[t])+'.pdf', 
-                format='pdf',
+                save_path+str(observed_time_points[t])+'.svg', 
+                format='svg',
                 dpi=300,                
                 bbox_inches='tight',    
                 pad_inches=0.1          
@@ -276,8 +283,8 @@ def visualize_drift_func(
         ax.set_aspect('equal', adjustable='box')
     if save_path is not None:
         plt.savefig(
-            save_path+'.pdf', 
-            format='pdf',
+            save_path+'.svg', 
+            format='svg',
             dpi=300,                
             bbox_inches='tight',    
             pad_inches=0.1          
@@ -293,13 +300,14 @@ def visualize_generated_trajectories(
     umap_model: Optional[Any] = None,
     adjust_equal:Optional[bool] = False,
     save_path: Optional[str] = None,
-    adjust_upside_down: Optional[bool] = False
+    adjust_upside_down: Optional[bool] = False,
+    figsize = (12, 10)
 ):
     steps = len(generated_data) - 1
     sim=torch.stack(generated_data)
     x_seq=torch.cat(raw_data).cpu().numpy()
     xu = umap_model.transform(x_seq) if umap_model is not None else x_seq
-    fig, ax = plt.subplots(figsize = (12, 10))
+    fig, ax = plt.subplots(figsize = figsize)
     ax.scatter(xu[:,0], xu[:,1] if not adjust_upside_down else -xu[:,1] + 9, s=1, color = 'gray')
     c = np.arange(steps+1)
     for i in np.random.randint(0, sim.shape[1], size = n_trajectories):
@@ -313,8 +321,8 @@ def visualize_generated_trajectories(
         ax.set_aspect('equal', adjustable='box')
     if save_path is not None:
         plt.savefig(
-            save_path+'.pdf', 
-            format='pdf',
+            save_path+'.svg', 
+            format='svg',
             dpi=300,                
             bbox_inches='tight',    
             pad_inches=0.1          
